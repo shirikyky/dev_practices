d59018d5cb013c9824c9d24b78e1395c82399020
shiriky <123shirit@gmail.com>
2026-07-09 20:45:21 +0300

    test

--- src/books.js
+++ src/books.js
@@ -1,7 +1,7 @@
 const books = [
   { id: 1, title: "Clean Code" },
   { id: 2, title: "You Don't Know JS" },
-  { id: 3, title: "The Pragmatic Programmer" },
+  { id: 4, title: "The Pragmatic Programmer" },
 ];
 
 console.log(books);
